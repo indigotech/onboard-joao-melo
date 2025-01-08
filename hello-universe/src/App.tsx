1b2b485964d258ea2eabb6f7cbd3b4d1a82e40a3
jofe2003 <joao.melo@taqtile.com.br>
2025-01-08 08:46:57 -0300

    - Text change - Pin Versions

--- hello-universe/src/App.tsx
+++ hello-universe/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="TitleText"> Hello Word! </h1>
+      <h1 className="TitleText"> Hello Universe! </h1>
     </div>
   );
 }
