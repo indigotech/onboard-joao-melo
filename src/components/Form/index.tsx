844986386682aff189c26fba372e49765b53b6d6
jofe2003 <joao.melo@taqtile.com.br>
2025-01-14 09:26:24 -0300

    - Implementation of the Error Message Component, - Full validation logic.

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -5,13 +5,14 @@ import { Container, Input, LabelInput } from './style';
 interface FormProps {
   name: string;
   info: string;
+  setValue?: (text: string) => void;
 }
 
 export function Form(props: FormProps): JSX.Element {
   return (
     <Container>
       <LabelInput> {props.name} </LabelInput>
-      <Input> {props.info} </Input>
+      <Input onChangeText={props.setValue}> {props.info} </Input>
     </Container>
   );
 }
